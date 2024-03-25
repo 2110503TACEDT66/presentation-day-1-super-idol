2b9d7e2e954e408e974e9f56cdac2285153e0545
tewpongamorn123 <tewpongamorn@gmail.com>
2024-03-26 00:31:52 +0700

    Joboffer

--- models/Joboffer.js
+++ models/Joboffer.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
 const JobofferSchema = new mongoose.Schema({
+    company: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Company',
+        required: true
+    },
     name: {
         type: String,
         required: [true, 'Please add a name'],
