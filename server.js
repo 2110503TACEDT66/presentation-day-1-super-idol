d560b3f63c7b374cc2297099a5bc395c73ca5a17
tewpongamorn123 <tewpongamorn@gmail.com>
2024-03-25 19:01:46 +0700

    Joboffer

--- server.js
+++ server.js
@@ -22,7 +22,13 @@ app.use('/api/v1/auth', auth);
 
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(
+    PORT, 
+    console.log('Server running in',
+    process.env.NODE_ENV,
+    'on ' + process.env.HOST + ':' + PORT
+    )
+);
 
 process.on('unhandledRejection', (err, promise) => {
     console.log(`Error: ${err.message}`);
